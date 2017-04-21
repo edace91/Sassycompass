450ee0a7e8463de254f11988aac1b400cb0f437e
edace91 <eduace91@gmail.com>
2017-04-20 20:56:55 -0500

    gruntfile edit

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
